df8870a360cff3384b4cbf657fc00cdecac3ffff
Mauro <mauro69@gmail.com>
2022-10-11 00:58:01 +0200

    Update main.tf

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -75,7 +75,7 @@ resource "aws_instance" "rosa-bastion" {
 #!/bin/bash
 
 dnf -e 0 -q -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-dnf -e 0 -q -y install git iperf3 podman wget jq bind-utils make zsh
+dnf -e 0 -q -y install git iperf3 podman wget jq bind-utils make zsh net-tools
 wget -q https://github.com/mikefarah/yq/releases/download/v4.27.5/yq_linux_amd64.tar.gz -O - | tar xvzf - -C /usr/local/bin --strip-components=0 ./yq_linux_amd64
 mv /usr/local/bin/yq_linux_amd64 /usr/local/bin/yq
 wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
