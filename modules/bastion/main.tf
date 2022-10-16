1046190d4c9605f44644ccb44f07684c1cc5e85a
Mauro <mauro69@gmail.com>
2022-10-16 22:35:45 +0200

    Update main.tf

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -76,6 +76,8 @@ resource "aws_instance" "rosa-bastion" {
 
 dnf -e 0 -q -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf -e 0 -q -y install git iperf3 podman wget jq bind-utils make zsh net-tools
+
+
 wget -q https://github.com/mikefarah/yq/releases/download/v4.27.5/yq_linux_amd64.tar.gz -O - | tar xvzf - -C /usr/local/bin --strip-components=0 ./yq_linux_amd64
 mv /usr/local/bin/yq_linux_amd64 /usr/local/bin/yq
 wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
@@ -85,9 +87,10 @@ mv oc kubectl /usr/local/bin/
 
 sudo -u ec2-user -i bash <<_EC2USER_
 id -a
-git clone https://github.com/mauroseb/dotfiles.git
+git clone --depth 1 https://github.com/mauroseb/dotfiles.git
 cd dotfiles ; make all; cd
 usermod -s `which zsh` ec2-user
+git clone --depth 1 https://github.com/asdf-vm/asdf.git ~/.asdf
 exit
 _EC2USER_
 
