b4b237b7331c42ebd7e6dc72472f7ddaf39460e9
Mauro <mauro69@gmail.com>
2022-10-21 10:14:06 +0200

    Update main.tf

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -71,31 +71,7 @@ resource "aws_instance" "rosa-bastion" {
     key_name                      = aws_key_pair.bastion-keypair.key_name
     subnet_id                     = aws_subnet.bastion-subnet.id
     vpc_security_group_ids        = [aws_security_group.bastion-sg.id]
-    user_data = <<EOF
-#!/bin/bash
-
-dnf -e 0 -q -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-dnf -e 0 -q -y install git iperf3 podman wget jq bind-utils make zsh net-tools
-
-
-wget -q https://github.com/mikefarah/yq/releases/download/v4.27.5/yq_linux_amd64.tar.gz -O - | tar xvzf - -C /usr/local/bin --strip-components=0 ./yq_linux_amd64
-mv /usr/local/bin/yq_linux_amd64 /usr/local/bin/yq
-wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
-curl -sSL4 https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64 -o /usr/local/bin/helm && chmod +x /usr/local/bin/helm
-tar xzf openshift-client-linux.tar.gz kubectl oc && rm openshift-client-linux.tar.gz
-mv oc kubectl /usr/local/bin/
-
-sudo -u ec2-user -i bash <<_EC2USER_
-id -a
-git clone --depth 1 https://github.com/mauroseb/dotfiles.git
-cd dotfiles ; make all; cd
-usermod -s `which zsh` ec2-user
-git clone --depth 1 https://github.com/asdf-vm/asdf.git ~/.asdf
-exit
-_EC2USER_
-
-
-EOF
+    user_data                     = templatefile("user_data.sh", {username = "ec2-user"})
     tags                          = {
         Owner = var.cluster_owner_tag
         Name  = "${var.env_name}-bastion"
